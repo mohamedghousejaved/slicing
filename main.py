7cb07ed202746074079624f923bb2fe00b495b29
MohamadGhouseJaved <mohamedghousejaved@gmail.com>
2022-12-16 19:20:15 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# slicing from index start to index stop-1
+# array[start:stop]
+
+# slicing from index start to the end
+# array[start:]
+
+# slicing from the beginning to index stop - 1
+# array[:stop]
+
+# slicing from the index start to index stop, by skipping step
+# array[start:stop:step]
+
